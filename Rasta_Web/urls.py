683fef58ecd90ca2f6d5856344577522dd302eea
arghavan99 <a.rezvani99@yahoo.com>
2020-03-27 12:14:11 +0430

    handle basic urls for hamepage

--- Rasta_Web/urls.py
+++ Rasta_Web/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from apps.intro import urls as intro_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('', include(intro_urls), name='intro'),
 ]
