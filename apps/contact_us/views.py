b7506c69127ebc1c1dbbce3981a40f2edc095d95
arghavan99 <a.rezvani99@yahoo.com>
2020-03-31 17:08:50 +0430

    get members for contact_us page

--- apps/contact_us/views.py
+++ apps/contact_us/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from apps.contact_us.models import RastaMember
+
 
 def get_members(request):
-    return render(request, 'contact_us/contact_us.html')
+    context = {}
+    if request.method == 'GET':
+        members = RastaMember.objects.all().order_by('id')
+        context = {'members': members}
+    return render(request, 'contact_us/contact_us.html', context)
+
