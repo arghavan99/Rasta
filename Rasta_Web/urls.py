d544f808f07fa2b82fba938be434062d77edb25e
ShadiGhasemi <shadighasemi.sg@gmail.com>
2020-04-03 03:47:00 +0430

    ContanctUs Members

--- Rasta_Web/urls.py
+++ Rasta_Web/urls.py
@@ -26,3 +26,7 @@ urlpatterns = [
     path('contact_us/', include(contact_us_urls)),
     path('', include(intro_urls)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
