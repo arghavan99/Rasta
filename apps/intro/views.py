6010d94f71b00be06ea7286673839e26e5235d3f
arghavan99 <a.rezvani99@yahoo.com>
2020-04-03 11:57:50 +0430

    context

--- apps/intro/views.py
+++ apps/intro/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from apps.intro.models import *
 
 
 def homepage(req):
-    return render(req, 'intro/index.html')
+    upcoming_event = UpcomingEvent.objects.last()
+    homepage_data = HomepageData.objects.last()
+    context = {'homepage_data': homepage_data,
+               'upcoming_event': upcoming_event}
+    return render(req, 'intro/index.html', context)
