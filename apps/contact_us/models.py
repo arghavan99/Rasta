fd9fd923073548ab7765025b41cf251adb191582
arghavan99 <a.rezvani99@yahoo.com>
2020-04-03 15:38:05 +0430

    image resize

--- apps/contact_us/models.py
+++ apps/contact_us/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-from django.utils.translation import ugettext, ugettext_lazy as _
-
+from PIL import Image
+from io import BytesIO
+from django.core.files.uploadedfile import InMemoryUploadedFile
+import sys
 
 # Create your models here.
 
@@ -11,6 +13,26 @@ class RastaMember(models.Model):
     role = models.CharField(max_length=100, blank=True, null=True)
     photo_visible = models.ImageField(null=False, blank=False)
     photo_hidden = models.ImageField(null=False, blank=False)
+
+    def save(self):
+        im1 = Image.open(self.photo_visible)
+        im2 = Image.open(self.photo_hidden)
+        output1 = BytesIO()
+        output2 = BytesIO()
+        im1 = im1.resize((150, 150))
+        im2 = im2.resize((150, 150))
+        im1.save(output1, format='png', quality=100)
+        im2.save(output2, format='png', quality=100)
+        output1.seek(0)
+        output2.seek(0)
+        self.photo_visible = InMemoryUploadedFile(output1, 'ImageField', "%s.jpg" % self.photo_visible.name.split('.')[0], 'image/jpeg',
+                                        sys.getsizeof(output1), None)
+        self.photo_hidden = InMemoryUploadedFile(output2, 'ImageField', "%s.jpg" % self.photo_hidden.name.split('.')[0], 'image/jpeg',
+                                        sys.getsizeof(output2), None)
+        super(RastaMember, self).save()
+
+    def __str__(self):
+        return self.name + ' - ' + self.role
 
 
 class UserFeedback(models.Model):
@@ -23,3 +45,6 @@ class UserFeedback(models.Model):
     email = models.EmailField(null=False, blank=False)
     type = models.CharField(max_length=200, null=False, blank=False, choices=type_choices)
     submit_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.submit_time) + ' - ' + self.type
