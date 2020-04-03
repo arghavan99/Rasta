c0546369485de92966c01988343b955758dc1604
arghavan99 <a.rezvani99@yahoo.com>
2020-04-03 12:16:14 +0430

    aparat url

--- apps/intro/views.py
+++ apps/intro/views.py
@@ -7,6 +7,9 @@ from apps.intro.models import *
 def homepage(req):
     upcoming_event = UpcomingEvent.objects.last()
     homepage_data = HomepageData.objects.last()
-    context = {'homepage_data': homepage_data,
+    link = homepage_data.video_url.split('/')
+    url = 'https://www.aparat.com/video/video/embed/videohash/' + str(link[-1]) + '/vt/frame'
+    context = {'intro': homepage_data.intro,
+               'aparat_url': url,
                'upcoming_event': upcoming_event}
     return render(req, 'intro/index.html', context)
