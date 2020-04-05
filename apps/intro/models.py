0386ec92875edd135ae17a305775c92d51946e1f
arghavan99 <a.rezvani99@yahoo.com>
2020-04-05 14:55:11 +0430

    add field show_on_homepage to upcoming event

--- apps/intro/models.py
+++ apps/intro/models.py
@@ -16,3 +16,4 @@ class UpcomingEvent(models.Model):
     title = models.CharField(max_length=100, null=False, blank=False)
     button_link = models.URLField(null=False, blank=False)
     button_name = models.CharField(null=False, blank=False, max_length=50)
+    show_on_homepage = models.BooleanField(default=True, null=False, blank=False)
--- apps/intro/views.py
+++ apps/intro/views.py
@@ -5,11 +5,15 @@ from apps.intro.models import *
 
 
 def homepage(req):
-    upcoming_event = UpcomingEvent.objects.last()
+    upcoming_event = UpcomingEvent.objects.filter(show_on_homepage=True)
+    if len(upcoming_event) > 0:
+        context = {'upcoming_event': upcoming_event.last()}
+    else:
+        context = {}
     homepage_data = HomepageData.objects.last()
     link = homepage_data.video_url.split('/')
     url = 'https://www.aparat.com/video/video/embed/videohash/' + str(link[-1]) + '/vt/frame'
-    context = {'intro': homepage_data.intro,
-               'aparat_url': url,
-               'upcoming_event': upcoming_event}
+    context.update({'intro': homepage_data.intro,
+                    'aparat_url': url})
     return render(req, 'intro/index.html', context)
+
