aa02cf2cc54532b7fff6e74eded78c2aa35f612f
R Dehghan <rdb1377@gmail.com>
2020-04-15 00:31:33 +0430

    image resize

--- apps/events/models.py
+++ apps/events/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from ckeditor.fields import RichTextField
-
+from PIL import Image
+from io import BytesIO
+from django.core.files.uploadedfile import InMemoryUploadedFile
+import sys
 
 class Event(models.Model):
     name = models.CharField(max_length=200, null=False, blank=False)
@@ -9,6 +12,21 @@ class Event(models.Model):
     description = RichTextField(null=False, blank=False)
     location = models.CharField(max_length=100, null=True, blank=True) #todo false
     date = models.DateField(null=True, blank=True) # todo false
+
+    def save(self):
+        im1 = Image.open(self.poster)
+
+        output1 = BytesIO()
+        im1 = im1.resize((300, 400))
+        im1.save(output1, format='png', quality=100)
+
+        output1.seek(0)
+
+        self.poster = InMemoryUploadedFile(output1, 'ImageField',
+                                                  "%s.jpg" % self.poster.name.split('.')[0], 'image/jpeg',
+                                                  sys.getsizeof(output1), None)
+
+        super(Event, self).save()
 
     def __str__(self):
         return self.name
