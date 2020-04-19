42cb8800500aa6ee36bf8af68aa8153ec62bfc4a
ShadiGhasemi <shadighasemi.sg@gmail.com>
2020-04-19 15:55:51 +0430

    Adding events to homepage view

--- apps/intro/views.py
+++ apps/intro/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from apps.intro.models import *
+from apps.events.models import Event
 
 
 def homepage(req):
+    get_last_events()
     upcoming_event = UpcomingEvent.objects.filter(show_on_homepage=True)
     if len(upcoming_event) > 0:
         context = {'upcoming_event': upcoming_event.last()}
@@ -12,6 +14,10 @@ def homepage(req):
     link = homepage_data.video_url.split('/')
     url = 'https://www.aparat.com/video/video/embed/videohash/' + str(link[-1]) + '/vt/frame'
     context.update({'intro': homepage_data.intro,
-                    'aparat_url': url})
+                    'aparat_url': url,
+                    'events': get_last_events()})
     return render(req, 'intro/index.html', context)
 
+
+def get_last_events():
+    return Event.objects.order_by('-date')[:3]
