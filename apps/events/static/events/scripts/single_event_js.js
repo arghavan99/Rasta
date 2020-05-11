85fb4b4290573e9e1fea75a769e604d8055c49ac
mahtabbigverdi <bigverdi.1377@gmail.com>
2020-05-11 23:51:59 +0330

    debug event page

--- apps/events/static/events/scripts/single_event_js.js
+++ apps/events/static/events/scripts/single_event_js.js
@@ -1,3 +1,15 @@
+
+$(window).load(function () {
 $('.special.cards .image').dimmer({
   on: 'hover'
 });
+});
+
+$(window).resize(toggle);
+$(window).load(toggle);
+
+
+function toggle () {
+      var parentwidth = $(".parent").width();
+      $('.child').css('max-width', parentwidth+"px");
+  }
