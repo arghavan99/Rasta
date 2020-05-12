1679629ba7dc52cdfe9523e7291196f41ce3b056
ShadiGhasemi <shadighasemi.sg@gmail.com>
2020-05-12 09:51:56 +0430

    responsive single event

--- apps/events/static/events/scripts/single_event_js.js
+++ apps/events/static/events/scripts/single_event_js.js
@@ -1,30 +1,25 @@
-
 $(window).load(function () {
 $('.special.cards .image').dimmer({
   on: 'hover'
 });
 });
-$(window).resize(toggle);
-$(window).load(toggle);
+
 var distance = 100;
 
 $(window).scroll(function () {
- if ( $(window).scrollTop() >= distance ) {
-        $('.child').css('top', "10%");}
-});
-$(window).scroll(function () {
- if ( $(window).scrollTop() <= distance ) {
-        $('.child').css('top', "30%");}
+    if ($(window).width() >= 768) {
+        if ( $(window).scrollTop() >= distance ) {
+            $('.child').css('top', "15%");
+        } else {
+            $('.child').css('top', "30%");
+        }
+    }
 });
 
 function toggle () {
       var parentwidth = $(".parent").width();
       $('.child').css('max-width', parentwidth+"px");
-  }
+}
 
-
-
-
-
-
-
+$(window).resize(toggle);
+$(window).load(toggle);
