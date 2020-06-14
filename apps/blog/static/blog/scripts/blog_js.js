9518302e00097e7879d634135b811e9c4575813c
R Dehghan <rdb1377@gmail.com>
2020-06-14 11:50:33 +0430

    comment reply fix bibot bug

--- apps/blog/static/blog/scripts/blog_js.js
+++ apps/blog/static/blog/scripts/blog_js.js
@@ -9,11 +9,3 @@ $(window).load(function () {
   on: 'hover'
 });
 });
-
-function create_reply_form(comment_id) {
-    var reply_form = document.createElement('div');
-    reply_form.class = 'ui stackable grid';
-//    reply_form.innerHTML = '<p>hii</p>';
-    reply_form.innerHTML = '<form id="blog_form" method="post" class="ui form error" style="margin:auto;width: 90%"><input type="text" name="author_name"></form>'
-    document.getElementById('cm_' + comment_id).appendChild(reply_form);
-}
