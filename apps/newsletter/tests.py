15e1a977f1c42c8975542133f5b83c1748101f45
ShadiGhasemi <shadighasemi.sg@gmail.com>
2020-06-17 15:05:10 +0430

    test

--- apps/newsletter/tests.py
+++ apps/newsletter/tests.py
@@ -1,22 +1,22 @@
+from django.db import IntegrityError
 from django.test import TestCase
 from apps.newsletter import views
 from apps.newsletter.models import *
 import jdatetime
 
 
-class newsletterTest(TestCase):
+class NewsletterTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
         sub1 = Subscriber.objects.create(email='valid.email@email.com')
 
-
     def test_duplicate_emails_not_allowed(self):
         try:
             sub1 = Subscriber.objects.create(email='valid.email@email.com')
-            sub2 = Subscriber.objects.create(email='valid.email@email.com')
-        except:
-            Exception
+            self.fail()
+        except IntegrityError:
+            pass
 
     def test_subscriber_date(self):
         sub = Subscriber.objects.get(id=1)
@@ -26,5 +26,6 @@ class newsletterTest(TestCase):
 
     def test_response_successful(self):
         sub = Subscriber.objects.get(id=1)
-        response = views.unsubscribe(print() ,sub.unique_id)
+        response = views.unsubscribe(print(), sub.unique_id)
         self.assertEqual(response.status_code, 200)
+        self.assertRaises(Exception, Subscriber.objects.get, unique_id=sub.unique_id)
