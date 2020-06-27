a6fc9be1b6e1ed30f57fe9a60ecb468d2b52d6c3
arghavan99 <a.rezvani99@yahoo.com>
2020-06-27 22:27:33 +0430

    settings

--- Rasta_Web/utils.py
+++ Rasta_Web/utils.py
@@ -1,6 +1,6 @@
 from django.core.exceptions import ValidationError
 
-from Rasta_Web.settings.settings import bibot_SiteSecretKey
+from django.conf import settings
 import requests
 from django.contrib import messages
 
@@ -9,7 +9,7 @@ def check_bibot_response(request):
     if request.POST.get('bibot-response') is not None:
         if request.POST.get('bibot-response') != '':
             r = requests.post('https://api.bibot.ir/api1/siteverify/', data={
-                'secret': bibot_SiteSecretKey,
+                'secret': settings.bibot_SiteSecretKey,
                 'response': request.POST['bibot-response']
             })
             print(r.json())
